3ee15912f68d3a66e77c1650f057205143e85867
pradeepm <pradeepm@brykgroup.com>
2023-01-17 15:51:36 +1100

    Commented provider block

--- modules/bastion-session.tf
+++ modules/bastion-session.tf
@@ -9,7 +9,10 @@ resource "oci_bastion_session" "create_session" {
   bastion_id = oci_bastion_bastion.new_bastion.id
   key_details {
     #Required
-    public_key_content = file("C:/PradeepM/SSH-keys/Oracle-Bastion/ssh-key-2022-12-06.key.pub")
+    //public_key_content = file("C:/PradeepM/SSH-keys/Oracle-Bastion/ssh-key-2022-12-06.key.pub")
+    public_key_content = <<-EOF
+    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBen3EilHRsdwmv5s9lJ30Fqlpy2kVUtkEp4ctDYDnLPVINSTIY1zUqZ0rIYswocbVJC8TvEtXZBnyjlqBtuAVUv0k/+IRWAa7wMC2EX7Fa8nRRS9KUA7cW2C1dBa5M6xg6fkt7GCh9XNE96jVojsAMf1vvVy5hIr6QcCZHI6KnbaiEJ9Un4P+pZbRvg5fSTC8RN7wacWlEnYwPIay6HPFYLb1iTnBcG38Piz9vwcf1c/OLPwFYTib074mwtrQ6oxNfyVlyafcrnrFtmFE6YuZFbCwu3chaxPYYbc9FSRQ0/iFBBQVCrAtcjgKQyQGOx3q1hm2Qkb4mTthlEBtAHxd ssh-key-2022-12-06
+    EOF
   }
   target_resource_details {
     #Required
